01d4fe915f0900dc1360b4acaa40e6208ac461e5
Kimskaya <120120877+Kimskaya@users.noreply.github.com>
2023-02-05 17:16:21 +0300

    Update Program.cs

--- task_62_var/Program.cs
+++ task_62_var/Program.cs
@@ -1,16 +1,40 @@
-﻿// 
+// task_62 . Fill in array 4 by 4 spirally
+// 1   2  3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10  9 8  7
 int[,] array = new int[4,4];
 
-int temp = 1;
+int element = 1;
 int i = 0;
 int j = 0;
-
-while (temp <= array.GetLength(0) * array.GetLength(1))
+array[i, j] = element;
+while (element <= array.GetLength(0) * array.GetLength(1))
 {
- for ( j = 0; i <= array.GetLength(0)-(i+1); j++)
-   
-    array[i, j] = temp;
-    temp++; 
+  int rowOrColLength = i+j;
+  element++;
+  if (i <=j+1 && rowOrColLength < array.GetLength(0) - 1)
+  {
+    j++;
+    array[i, j] = element;
+  }
+  else if (i < j && rowOrColLength >= array.GetLength(0) - 1)
+  {
+    i++;
+    array[i, j] = element;
+  }
+  else if (i >= j && rowOrColLength > array.GetLength(0) - 1)
+   {
+    j--;
+    array[i, j] = element;
+   }
+  else
+  {
+    i--;
+    array[i, j] = element;
+  }
+}
+
 PrintArray(array);
 
 void PrintArray (int[,] array)
@@ -19,12 +43,8 @@ void PrintArray (int[,] array)
   {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-      if (array[i,j] / 10 <= 0)
-      Console.Write($" {array[i,j]} ");
-
-      else Console.Write($"{array[i,j]} ");
+      Console.Write($"   {array[i,j]}      ");
     }
     Console.WriteLine();
   }
-}
 }
